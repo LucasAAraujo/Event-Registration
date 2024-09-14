a34d39596022db14304dd9ba85473d694c129dce
Lucas Alves <lucasalves3.0@hotmail.com>
2024-09-14 08:51:27 -0300

    Tailwind instaled

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}",
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
