0a79a1b28127e1f4121befeef9c20738fa4fa689
Lucas Alves <lucasalves3.0@hotmail.com>
2024-09-21 09:01:50 -0300

    feat: redereing and styling EventCard component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <main class="container mx-auto my-8 space-y-8 text-red-600">
-    Hello! 
+  <main class="container mx-auto my-8 space-y-8">
+    <h1 class="text-4xl font-medium">Event Booking App</h1> 
+    <h2 class="text-2xl font-medium">All Events</h2>
+    <section class="grid grid-cols-2 gap-8">
+      <EventCard v-for="i in 6" :key="i" />
+    </section>
+    <h2 class="text-2xl font-medium">Your Bookings</h2>
   </main>
 </template>
+
+
+<script setup>
+import EventCard from '@/components/EventCard.vue';
+</script>
