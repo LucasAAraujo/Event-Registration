b2f16573849e277141ee27b3f2061fd915e89960
Lucas Alves <lucasalves3.0@hotmail.com>
2024-09-21 09:08:46 -0300

    feat: Passing props

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,13 +1,23 @@
 <template>
     <div class="border border-gray-200 bg-white rounded-md">
         <h3 class="p-4 font-medium text-xl border-b border-gray-200">
-            What
+            {{ title }}
         </h3>
         <p class="p-4 border-b border-gray-200">
-            When
+            {{ when }}
         </p>
         <p class="p-4">
-            Description
+            {{ description }}
         </p>
     </div>
 </template>
+
+<script setup>
+
+    defineProps({
+        title: String,
+        when: String,
+        description: String
+    });   
+
+</script>
